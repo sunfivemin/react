36e52552368a2202a5175518850aae2f4d12cdaf
sunfivemin <sunfivemin@gmail.com>
2024-08-01 09:20:40 +0900

    🎨 redux 장바구니

--- shop/src/pages/Cart.js
+++ shop/src/pages/Cart.js
@@ -1,32 +1,62 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { changeName } from "./../store.js";
 
 function Cart() {
-    let user = useSelector((state) => {
-        return state;
-    });
+  // 필요한 상태만 선택합니다.
+  const user = useSelector((state) => state.user);
+  const cart = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
 
-    return (
-        <div class="container mx-auto p-4 mt-10">
-            <table class="min-w-full bg-white border border-gray-200 rounded-lg shadow">
-                <thead class="bg-indigo-500 text-white">
-                    <tr>
-                        <th class="py-3 px-4 text-center text-sm font-semibold uppercase">#</th>
-                        <th class="py-3 px-4 text-center text-sm font-semibold uppercase">상품명</th>
-                        <th class="py-3 px-4 text-center text-sm font-semibold uppercase">수량</th>
-                        <th class="py-3 px-4 text-center text-sm font-semibold uppercase">변경하기</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr class="border-t border-gray-200 hover:bg-gray-100">
-                        <td class="py-3 px-4 text-center text-sm text-gray-700">1</td>
-                        <td class="py-3 px-4 text-center text-sm text-gray-700">안녕</td>
-                        <td class="py-3 px-4 text-center text-sm text-gray-700">안녕</td>
-                        <td class="py-3 px-4 text-center text-sm text-gray-700">안녕</td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-    );
+  return (
+    <div className="container mx-auto p-4 mt-10">
+      <h6>{user.name}의 장바구니</h6>
+      <table className="min-w-full bg-white border border-gray-200 rounded-lg shadow">
+        <thead className="bg-indigo-500 text-white">
+          <tr>
+            <th className="py-3 px-4 text-center text-sm font-semibold uppercase">
+              번호
+            </th>
+            <th className="py-3 px-4 text-center text-sm font-semibold uppercase">
+              상품명
+            </th>
+            <th className="py-3 px-4 text-center text-sm font-semibold uppercase">
+              수량
+            </th>
+            <th className="py-3 px-4 text-center text-sm font-semibold uppercase">
+              <button
+                onClick={() => {
+                  dispatch(changeName());
+                }}
+              >
+                버튼임
+              </button>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {cart.map((item, index) => (
+            <tr
+              className="border-t border-gray-200 hover:bg-gray-100"
+              key={index}
+            >
+              <td className="py-3 px-4 text-center text-sm text-gray-700">
+                {index + 1}
+              </td>
+              <td className="py-3 px-4 text-center text-sm text-gray-700">
+                {item.name}
+              </td>
+              <td className="py-3 px-4 text-center text-sm text-gray-700">
+                {item.count}
+              </td>
+              <td className="py-3 px-4 text-center text-sm text-gray-700">
+                안녕
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default Cart;
--- shop/src/store.js
+++ shop/src/store.js
@@ -1,12 +1,29 @@
 import { configureStore, createSlice } from "@reduxjs/toolkit";
 
 let user = createSlice({
-    name: "user",
-    initialState: "min",
+  name: "user",
+  initialState: { name: "kim", age: 20 },
+  reducers: {
+    changeName(state) {
+      state.name = "park";
+    },
+  },
 });
 
+export let { changeName } = user.actions;
+
+let cart = createSlice({
+  name: "cart",
+  initialState: [
+    { id: 0, name: "White and Black", count: 2 },
+    { id: 2, name: "Grey Yordan", count: 1 },
+  ],
+});
+
+// 등록
 export default configureStore({
-    reducer: {
-        user: user.reducer,
-    },
+  reducer: {
+    user: user.reducer,
+    cart: cart.reducer,
+  },
 });
